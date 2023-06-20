afc10bab2aedbee2f8d99c148ad0a987fafc9e4b
Pisklova Marina <pisklovama@gmail.com>
2023-06-20 11:34:35 +0500

    Initial commit

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export interface AboutPageProps {}
+
+const AboutPage: FC<AboutPageProps> = (props) => {
+  return <div>About Page</div>;
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export interface MainPageProps {}
+
+const MainPage: FC<MainPageProps> = (props) => {
+  return <div>Main Page</div>;
+};
+
+export default MainPage;
