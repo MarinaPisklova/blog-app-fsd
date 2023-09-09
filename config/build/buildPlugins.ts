13b66ae0bf356dca3eed57412a24d3ba7e600574
Pisklova Marina <pisklovama@gmail.com>
2023-09-09 13:57:11 +0500

    Fixed dev config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,7 +9,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -21,10 +21,18 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin({ overlay: false }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
+
+    // new ReactRefreshWebpackPlugin({ overlay: false }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })
+    );
+  }
+
+  return plugins;
 }
