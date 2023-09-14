8213ab6dd85f9ca3fa63438784854f235371fda1
Pisklova Marina <pisklovama@gmail.com>
2023-09-14 17:38:34 +0500

    add reg-cli, fix screenshots

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,7 +4,6 @@ import LangSwitcher from 'shared/ui/LangSwitcher/LangSwitcher';
 import Button, { ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { useTranslation } from 'react-i18next';
-
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
