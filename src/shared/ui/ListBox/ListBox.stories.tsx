fc57a6832a90f276deb97a8853f6880145ff459e
Pisklova Marina <pisklovama@gmail.com>
2024-09-15 13:21:37 +0500

    add dropdown, update screenshoots

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -4,15 +4,63 @@ import { ListBox } from './ListBox';
 export default {
   title: 'shared/ListBox',
   component: ListBox,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: 100 }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Normal = Template.bind({});
+Normal.args = {
+  defaultValue: 'Укажите значение',
+  items: [
+    { value: '1', content: 'Первый пункт' },
+    { value: '2', content: 'Второй пункт', disabled: true },
+    { value: '3', content: 'Третий пункт' },
+  ],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+  direction: 'top left',
+  defaultValue: 'Укажите значение',
+  items: [
+    { value: '1', content: 'Первый пункт' },
+    { value: '2', content: 'Второй пункт', disabled: true },
+    { value: '3', content: 'Третий пункт' },
+  ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+  direction: 'top right',
+  defaultValue: 'Укажите значение',
+  items: [
+    { value: '1', content: 'Первый пункт' },
+    { value: '2', content: 'Второй пункт', disabled: true },
+    { value: '3', content: 'Третий пункт' },
+  ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+  direction: 'bottom left',
+  defaultValue: 'Укажите значение',
+  items: [
+    { value: '1', content: 'Первый пункт' },
+    { value: '2', content: 'Второй пункт', disabled: true },
+    { value: '3', content: 'Третий пункт' },
+  ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+  direction: 'bottom right',
   defaultValue: 'Укажите значение',
   items: [
     { value: '1', content: 'Первый пункт' },
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -9,6 +9,8 @@ import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
+import { Dropdown } from 'shared/ui/Dropdown/Dropdown';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
 import cls from './Navbar.module.scss';
 
 export interface NavbarProps {
@@ -44,9 +46,21 @@ export const Navbar = memo(({ className }: NavbarProps) => {
         >
           {t('Создать статью')}
         </AppLink>
-        <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onLogOut}>
-          {t('Выйти')}
-        </Button>
+        <Dropdown
+          direction="bottom left"
+          className={cls.dropdown}
+          items={[
+            {
+              content: t('Профиль'),
+              href: RoutePath.profile + authData.id,
+            },
+            {
+              content: t('Выйти'),
+              onClick: onLogOut,
+            },
+          ]}
+          trigger={<Avatar size={30} src={authData.avatar} />}
+        />
       </header>
     );
   }
