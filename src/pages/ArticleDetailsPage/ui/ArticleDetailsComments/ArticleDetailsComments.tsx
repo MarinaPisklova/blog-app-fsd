e5e9ba44804c5936cf086b3e574b9d3c6dc6d271
Pisklova Marina <pisklovama@gmail.com>
2024-11-02 17:36:36 +0300

    update redux

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
@@ -1,17 +1,18 @@
-import { useTranslation } from 'react-i18next';
 import { memo, Suspense, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import { getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
 import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text, TextSize } from '@/shared/ui/Text';
-import { AddCommentForm } from '@/features/AddCommentForm';
+import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
 import { CommentList } from '@/entities/Comment';
+import { AddCommentForm } from '@/features/AddCommentForm';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useInitialEffect } from '@/shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { VStack } from '@/shared/ui/Stack';
 import { Loader } from '@/shared/ui/Loader';
+import { VStack } from '@/shared/ui/Stack';
+import { Text, TextSize } from '@/shared/ui/Text';
 
 interface ArticleDetailsCommentsProps {
   className?: string;
@@ -23,7 +24,7 @@ export const ArticleDetailsComments = memo((props: ArticleDetailsCommentsProps) 
   const { t } = useTranslation();
   const comments = useSelector(getArticleComments.selectAll);
   const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
   const onSendComment = useCallback(
     (text: string) => {
