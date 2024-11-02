198e93d16c9cabf604d84a563dc06c025c756086
Pisklova Marina <pisklovama@gmail.com>
2024-11-02 18:33:14 +0300

    update storybook

--- src/features/ArticleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
+++ src/features/ArticleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories.tsx
@@ -1,4 +1,3 @@
-import withMock from 'storybook-addon-mock';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ArticleRecommendationsList } from './ArticleRecommendationsList';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
@@ -10,7 +9,6 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-  decorators: [withMock],
 } as ComponentMeta<typeof ArticleRecommendationsList>;
 
 const Template: ComponentStory<typeof ArticleRecommendationsList> = (args) => (
