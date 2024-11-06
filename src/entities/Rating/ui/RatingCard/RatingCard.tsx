c403e7d0e996f9bff7d437b375c1cb8541d47dde
Pisklova Marina <pisklovama@gmail.com>
2024-11-06 22:48:35 +0300

    add e2e tests

--- src/entities/Rating/ui/RatingCard/RatingCard.tsx
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -55,12 +55,17 @@ export const RatingCard = memo((props: RatingCardProps) => {
   const modalContent = (
     <>
       <Text title={feedbackTitle} />
-      <Input value={feedback} onChange={setFeedback} placeholder={t('Ваш отзыв')} />
+      <Input
+        data-testid="RatingCard.Input"
+        value={feedback}
+        onChange={setFeedback}
+        placeholder={t('Ваш отзыв')}
+      />
     </>
   );
 
   return (
-    <Card max className={classNames('', {}, [className])}>
+    <Card data-testid="RatingCard" max className={classNames('', {}, [className])}>
       <VStack align="center" gap="8">
         <Text title={starsCount ? t('Спасибо за оценку!') : title} />
         <StarRating selectedStars={starsCount} size={40} onSelect={onSelectStars} />
@@ -70,10 +75,16 @@ export const RatingCard = memo((props: RatingCardProps) => {
           <VStack max gap="32">
             {modalContent}
             <HStack max gap="16" justify="end">
-              <Button onClick={cancelHandle} theme={ButtonTheme.OUTLINE_RED}>
+              <Button
+                data-testid="RatingCard.Close"
+                onClick={cancelHandle}
+                theme={ButtonTheme.OUTLINE_RED}
+              >
                 {t('Закрыть')}
               </Button>
-              <Button onClick={acceptHandle}>{t('Отправить')}</Button>
+              <Button data-testid="RatingCard.Send" onClick={acceptHandle}>
+                {t('Отправить')}
+              </Button>
             </HStack>
           </VStack>
         </Modal>
--- src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.tsx
@@ -46,14 +46,24 @@ const AddCommentForm = memo((props: AddCommentFormProps) => {
 
   return (
     <DynamicModuleLoader reducers={reducers}>
-      <HStack max justify="between" className={classNames(cls.AddCommentForm, {}, [className])}>
+      <HStack
+        data-testid="AddCommentForm"
+        max
+        justify="between"
+        className={classNames(cls.AddCommentForm, {}, [className])}
+      >
         <Input
+          data-testid="AddCommentForm.Input"
           className={cls.input}
           placeholder={t('Введите текст комментария')}
           value={text}
           onChange={onCommentTextChange}
         />
-        <Button theme={ButtonTheme.OUTLINE} onClick={onSendHandler}>
+        <Button
+          data-testid="AddCommentForm.Button"
+          theme={ButtonTheme.OUTLINE}
+          onClick={onSendHandler}
+        >
           {t('Отправить')}
         </Button>
       </HStack>
